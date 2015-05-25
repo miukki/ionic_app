ba682333a3848792d8a368e8a3e134cb1752c9cf
Anja Ishmukhametova <miukki@gmail.com>
2015-05-25 13:21:59 +0800

    changes bar and routes

--- www/js/app.js
+++ www/js/app.js
@@ -3,19 +3,28 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic']);
 
-.config(function($stateProvider, $urlRouterProvider) {
+
+app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/')
 
   $stateProvider.state('index', {
     url: '/',
-    templateUrl: 'index.html'
+    templateUrl: 'index.html',
+    controller: 'IndexCtrl'
   })
 
   $stateProvider.state('subs', {
     url: '/subs',
-    templateUrl: 'subs.html'
+    templateUrl: 'subs.html',
+    controller: 'SubsCtrl'
+  })
+
+  $stateProvider.state('upcoming', {
+    url: '/upcoming',
+    templateUrl: 'upcoming.html',
+    controller: 'UpcomingCtrl'
   })
 
 })
@@ -39,23 +48,27 @@ angular.module('starter', ['ionic'])
     all: function() {
       return [
         {'title': 'Subs Available', 'sref': 'subs'},
-        {'title': 'My Upcoming Classes', 'sref': 'subs'},
-        {'title': 'My Overdue Tasks', 'sref': 'subs'},
-        {'title': 'My Availability', 'sref': 'subs'},
-        {'title': 'My Finished Classes', 'sref': 'subs'},
-        {'title': 'My EF', 'sref': 'subs'}
+        {'title': 'My Upcoming Classes', 'sref': 'upcoming'},
+        {'title': 'My Overdue Tasks', 'sref': 'index'},
+        {'title': 'My Availability', 'sref': 'index'},
+        {'title': 'My Finished Classes', 'sref': 'index'},
+        {'title': 'My EF', 'sref': 'index'}
       ];
+    },
+    odds: function(){
+      return this.all().filter(function(v, i, arr){return i%2==0})
+    },
+    evens: function() {
+      return this.all().filter(function(v, i, arr){return i%2!=0})
     }
   }
 })
 
 
-.controller('MenuCtrl', function($scope, $timeout, $ionicModal, MenuF, $ionicSideMenuDelegate) {
+.controller('MainCtrl', function($scope, $timeout, $ionicModal, MenuF, $ionicSideMenuDelegate) {
 
   $scope.stateMenu = true;
   $scope.menu = MenuF.all();
-  $scope.menuOdds = $scope.menu.filter(function(v, i, arr){return i%2==0})
-  $scope.menuEvens = $scope.menu.filter(function(v, i, arr){return i%2!=0})
 
   // Create and load the Modal
   $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
@@ -70,22 +83,11 @@ angular.module('starter', ['ionic'])
     $scope.toggleMenu($scope.stateMenu);
   };
 
-  $scope.createTask = function(menu) {
-    $scope.menu.push({
-      title: menu.title
-    });
-    $scope.taskModal.hide();
-    menu.title = "";
-  };
   // Open our new task modal
   $scope.newTask = function() {
     $scope.taskModal.show();
   };
 
-  // Close the new task modal
-  $scope.closeNewTask = function() {
-    $scope.taskModal.hide();
-  };
 
   //open left menu
   $scope.toggleMenu = function(fl) {
@@ -103,6 +105,40 @@ angular.module('starter', ['ionic'])
       }
     }
   });
+
+
+})
+
+.controller('SubsCtrl', function($scope) {
+  console.log('SubsCtrl');
+})
+
+.controller('UpcomingCtrl', function($scope) {
+  console.log('UpcomingCtrl');
+})
+
+.controller('IndexCtrl', function($scope, MenuF) {
+
+  $scope.menuOdds = MenuF.odds();
+  $scope.menuEvens = MenuF.evens();
+
+})
+
+.controller('ModalCtrl', function($scope) {
+
+  $scope.createTask = function(menu) {
+    $scope.menu.push({
+      title: menu.title,
+      sref: 'index'
+    });
+    $scope.taskModal.hide();
+    menu.title = "";
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 
 
 });
